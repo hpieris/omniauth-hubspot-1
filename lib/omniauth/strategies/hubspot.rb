9ea798b5f59b5474a451edbb27c27fb579999005
Himakara Pieris <hpieris@gmail.com>
2018-09-15 03:37:56 -0700

    patch for full host port issue

--- lib/omniauth/strategies/hubspot.rb
+++ lib/omniauth/strategies/hubspot.rb
@@ -12,6 +12,18 @@ module OmniAuth
         authorize_url: 'https://app.hubspot.com/oauth/authorize',
         token_url: 'oauth/v1/token'
       }
+    
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
+      def full_host #https://github.com/omniauth/omniauth/issues/101
+        uri = URI.parse(request.url)
+        uri.path = ''
+        uri.query = nil
+        uri.port = (uri.scheme == 'https' ? 443 : 80)
+        uri.to_s
+      end
     end
   end
 end
